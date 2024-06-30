a6adbf7a93800677299be1d553e900194182ce51
Ankit Singh <rixronger@gmail.com>
2024-06-30 13:11:19 +0530

    Completed tweet controllers

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -8,7 +8,7 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const createTweet = asyncHandler(async (req, res) => {
 
     const { content } = req.body
-
+    console.log(content)
     if(!content){
         throw new ApiError(401, "Empty tweet, please write something in tweet.")
     }
@@ -31,11 +31,11 @@ const getUserTweets = asyncHandler(async (req, res) => {
 
     const { userId } = req.params
 
-    if(!userId){
+    if(!isValidObjectId(userId)){
         throw new ApiError(401, "Invalid user ID!")
     }
-
-    const tweet = await Tweet.findById(userId)
+    const query = {"owner" : userId}
+    const tweet = await Tweet.find(query)
 
     return res
     .status(200)
@@ -47,10 +47,60 @@ const getUserTweets = asyncHandler(async (req, res) => {
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+
+    // const {userId} = req.param
+
+    // if(!isValidObjectId(userId)){
+    //     throw new ApiError(401, "Unauthorized request")
+    // }
+
+    const { tweetId } = req.params
+
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(401, "Tweet ID not found!")
+    }
+
+    const {content} = req.body
+
+    if(!content){
+        throw new ApiError(401, "Please write something in tweet!")
+    }
+
+    const tweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            $set: {
+                content
+            }
+        },
+        {new: true}
+    )
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, tweet, "Tweet updated successfully!")
+    )
+
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+
+    const {tweetId} = req.params
+
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(401, "Tweet ID not found!")
+    }
+
+    await Tweet.findByIdAndDelete(tweetId)
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, {}, "Tweet Deleted!")
+    )
+
 })
 
 export {
